a152d642252b2e6e900499169f28218f087fdc90
HarryCandlish <hcnd90@gmail.com>
2019-06-25 14:22:00 +1200

    simple nav created

---
+++ src/components/Nav.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+import navStyles from "./nav.module.scss";
+
+class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <div>
+            <h4 className={navStyles.title}>All is Well.</h4>
+          </div>
+          <ul className={navStyles.navLinks}>
+            <a href="/">
+              <li>
+                <h4>Store</h4>
+              </li>
+            </a>
+            <a href="/">
+              <li>
+                {" "}
+                <h4>Events</h4>
+              </li>
+            </a>
+
+            <a href="/">
+              <li>
+                {" "}
+                <h4>News</h4>
+              </li>
+            </a>
+            <a href="/">
+              <li>
+                {" "}
+                <h4>Home</h4>
+              </li>
+            </a>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Nav;
